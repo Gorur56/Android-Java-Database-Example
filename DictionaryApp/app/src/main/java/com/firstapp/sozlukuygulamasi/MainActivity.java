427559ac687f717dba7777f6a02328d0ed853a90
Gorur56 <sevim.gorur01@gmail.com>
2024-03-24 19:45:24 +0300

    dictionaryApp files

--- DictionaryApp/app/src/main/java/com/firstapp/sozlukuygulamasi/MainActivity.java
+++ DictionaryApp/app/src/main/java/com/firstapp/sozlukuygulamasi/MainActivity.java
@@ -1,6 +1,8 @@
 package com.firstapp.sozlukuygulamasi;
 
 import android.os.Bundle;
+import android.provider.UserDictionary;
+import android.util.Log;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,8 +10,10 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
+public class MainActivity extends AppCompatActivity {
+    private DatabaseHelp db;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +24,22 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        db = new DatabaseHelp(this);
+
+        new Wordsdao().wordAdd(db,"door","kapı");
+        new Wordsdao().wordAdd(db,"window","pencere");
+        new Wordsdao().wordAdd(db,"sea","deniz");
+        new Wordsdao().wordAdd(db,"table","masa");
+        new Wordsdao().wordAdd(db,"pencil","kalem");
+
+
+        //We receive all the words we send
+        ArrayList<Words> incomingWordList = new Wordsdao().allWords(db);
+
+        for(Words w:incomingWordList)
+        {
+            Log.e(String.valueOf(w.getWord_id()), w.getEnglish()+"-"+w.getTurkish());
+        }
     }
 }
--- DictionaryApp/app/src/main/java/com/firstapp/sozlukuygulamasi/Wordsdao.java
+++ DictionaryApp/app/src/main/java/com/firstapp/sozlukuygulamasi/Wordsdao.java
@@ -40,5 +40,6 @@ public class Wordsdao {
 
             wordsArrayList.add(word);
         }
+        return wordsArrayList;
     }
 }
