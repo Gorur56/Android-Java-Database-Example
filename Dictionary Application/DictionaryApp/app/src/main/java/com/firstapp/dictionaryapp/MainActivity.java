e5b37b40b4e0021753335e875d5ef181e3b37892
Gorur56 <sevim.gorur01@gmail.com>
2024-04-03 22:35:52 +0300

    dictionary app design

---
+++ Dictionary Application/DictionaryApp/app/src/main/java/com/firstapp/dictionaryapp/MainActivity.java
@@ -0,0 +1,29 @@
+package com.firstapp.dictionaryapp;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MainActivity extends AppCompatActivity {
+    private Toolbar toolbar;
+    private RecyclerView rv;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        toolbar = findViewById(R.id.toolbar);
+        rv = findViewById(R.id.rv);
+    }
+}
