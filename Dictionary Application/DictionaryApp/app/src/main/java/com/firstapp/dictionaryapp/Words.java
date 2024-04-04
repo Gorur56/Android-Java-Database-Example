ee3e176a70cb7cf0e8d1aad7a39dc57fe81777c4
Gorur56 <sevim.gorur01@gmail.com>
2024-04-04 18:51:48 +0300

    create words class

---
+++ Dictionary Application/DictionaryApp/app/src/main/java/com/firstapp/dictionaryapp/Words.java
@@ -0,0 +1,40 @@
+package com.firstapp.dictionaryapp;
+
+public class Words {
+    private int word_id;
+    private String english;
+    private String turkish;
+
+    public Words() {
+    }
+
+    public Words(int word_id, String english, String turkish) {
+        this.word_id = word_id;
+        this.english = english;
+        this.turkish = turkish;
+    }
+
+    public int getWord_id() {
+        return word_id;
+    }
+
+    public void setWord_id(int word_id) {
+        this.word_id = word_id;
+    }
+
+    public String getEnglish() {
+        return english;
+    }
+
+    public void setEnglish(String english) {
+        this.english = english;
+    }
+
+    public String getTurkish() {
+        return turkish;
+    }
+
+    public void setTurkish(String turkish) {
+        this.turkish = turkish;
+    }
+}
