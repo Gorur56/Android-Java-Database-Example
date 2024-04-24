21afd8926aa5fcc1b9f7dfcd137877608a9e4ba1
Gorur56 <sevim.gorur01@gmail.com>
2024-04-24 17:36:05 +0300

    create veritabani class

---
+++ NotlarUygulamasi/app/src/main/java/com/firstapp/notlaruygulamasi/Veritabani.java
@@ -0,0 +1,31 @@
+package com.firstapp.notlaruygulamasi;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class Veritabani extends SQLiteOpenHelper {
+
+    public Veritabani(@Nullable Context context) {
+        super(context, "notlar.sqlite", null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE \"notlar\" (\n" +
+                "\t\"not_id\"\tINTEGER,\n" +
+                "\t\"ders_adi\"\tTEXT,\n" +
+                "\t\"Not1\"\tINTEGER,\n" +
+                "\t\"Not2\"\tINTEGER,\n" +
+                "\tPRIMARY KEY(\"not_id\" AUTOINCREMENT)\n" +
+                ");");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS notlar");
+        onCreate(db);
+    }
+}
